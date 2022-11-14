137244ac5f6433bd624a5cdb64fb4ee3be3fb043
oethington <o.ethington@gmail.com>
2022-11-14 09:01:08 -0600

    Final

--- parking-garage.py
+++ parking-garage.py
@@ -17,47 +17,72 @@
 # return to what action
 # increase space/ticket count by 1
 # remove user from storage
+            
 
 
 
 text = {
-    'enter' : "Enjoy your stay! Your ticket number is {ticket}",
+    'enter' : 'Enjoy your stay! Your ticket number is {ticket} ',
     'task_query' : 'Would you like to enter or exit the garage?: ',
     'exit' : 'Here is your total: {price}, Pay Now?',
-
+    'full' : "Sorry the garage is full. Try again later",
+    'ticketUse' : 'Number of available  tickets: ',
+    'availableSpace' : 'Number of available spaces: ',
+    'revenue' : 'Current revenue: '
 }
 
-ticket = [1, 2, 3, 4, 5]
-
-parkingSpaces = [1, 2, 3, 4, 5]
-
-currentTicket = {}
-
 class Garage():
-    def storedData(newTicket):
-        currentTicket.append({
-            'currentTicket': ticket
+    def __init__(self, price):
+        self.tickets = 0
+        self.spaces = 5
+        self.price = price
+        self.parkingSpaces = []
+        self.ticket_count = 5
+        self.currentTicket = {}
+    def storedData(self, newTicket):
+        self.currentTicket.append({
+            'ticket': self.ticket
         })
+    def show(self):
+        print(text['ticketUse'], self.ticket_count, ':', text['availableSpace'], self.spaces)
 
-    def enterGarage():
-        
+    def enterGarage(self):
+        if len(self.currentTicket) > self.ticket_count:
+            print(text['full'])
+        else:
+            
+            print('This is your parking space')
+            self.currentTicket['paid'] = False
+            self.tickets += 1
+            print(self.tickets)
+            self.decrease_ticket()
+            self.decrease_space()
+    def decrease_ticket(self):
+        self.ticket_count -= 1
+    def decrease_space(self):
+        self.spaces -= 1
+    def exitGarage(self):
+        payment_paid = input('please pay $6')
+        if payment_paid == '$6':
+            print('ticket has been paid and you have 15mins to leave.')
+            print('Thank you have a nice day!')
+        self.currentTicket['paid'] = True
+        self.increase_ticket()
+        self.increase_space()
+    def increase_ticket(self):
+        self.ticket_count += 1
+    def increase_space(self):
+        self.spaces += 1
+    def main(self):
+        while True: 
+            enter_or_exit = input(text['task_query'])
+            if enter_or_exit  == 'enter':
+                self.enterGarage()
+            elif enter_or_exit == 'exit':
+                self.exitGarage() 
+            elif enter_or_exit == 'admin':
+                self.show()
+                break
 
-    def exitGarage():
-        pass
-
-    def parkingCount():
-        pass
-
-    def main():
-        while True:
-            choice = input(text['task_query'])
-            choice = input(text['enter'])
-            storedData(currentTicket)
-            choice = input(text['exit'])
-
-            while True:
-                
-
-    main()
-
-
+badassGarage = Garage(6)
+badassGarage.main()
